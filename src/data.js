fa0ec91c4818e44eab73844cce2890326628a3e6
Claudia Tohalino <claudia.tohalino@gmail.com>
2023-04-26 18:51:38 -0500

    commit codigo terminado

--- src/data.js
+++ src/data.js
@@ -1,19 +1,25 @@
-//ORDENAR A - Z
-export const sortByAZ = (results) => {
-  const sortedData = results.sort((a, z) => (a.name > z.name ? 1 : -1));
+
+//FUNCION ORDENAR AZ
+export const funcionOrdenadoaZ = (datos) => {
+  const sortedData = datos.sort((a, z) => (a.name > z.name ? 1 : -1));
   return sortedData;
 };
-// eslint-disable-next-line no-console
-console.log(sortByAZ);
 
-//ORDENAR Z - A
-export const sortByZA = (results) => {
-  const sortedData = results.sort((a, z) => (a.name < z.name ? 1 : -1));
+//FUNCION ORDENAR ZA
+export const funcionOrdenadozA = (datos) => {
+  const sortedData = datos.sort((a, z) => (a.name < z.name ? 1 : -1));
   return sortedData;
 };
-// eslint-disable-next-line no-console
-console.log(sortByAZ);
 
-export const anotherExample = () => {
-  return 'OMG';
+//FUNCION FILTRAR POR GENERO
+export const filterGender = (characters, selectGender) => {
+  return characters.filter((results) => results.gender.includes(selectGender));
 };
+
+//FUNCION FILTRAR PARA CALCULO AGREGADO
+export const filterOrigen = (characters, selectOrigen) => {
+  return characters.filter((origin) => origin.name.includes(selectOrigen));
+};
+
+
+
